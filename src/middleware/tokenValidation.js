ce714051b76106eef347b8d40e31e3ee501bdde9
Jackson <jviana116@gmail.com>
2023-03-10 04:35:26 -0300

    feat: adicionando middleware para validação do token

---
+++ src/middleware/tokenValidation.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+const authToken = (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) return res.status(401).json({ message: 'Token not found' });
+  
+  try {
+    const { email } = jwt.verify(authorization, JWT_SECRET || 'secret');
+    req.auth = email;
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+  next();
+};
+
+module.exports = {
+  authToken,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 const userController = require("../controllers/userController");
+const { authToken } = require("../middleware/tokenValidation");
 
 const router = Router();
 
 router.post("/register", userController.createUser);
-router.post("/login", userController.login);
+router.post("/login", authToken, userController.login);
 
 module.exports = router;
