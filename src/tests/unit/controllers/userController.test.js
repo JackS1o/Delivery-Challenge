69362664f5666bdc495e8e2db2e6073b1e99e878
Jackson <jviana116@gmail.com>
2023-03-13 01:36:17 -0300

    feat: criando tests para login

--- src/tests/unit/controllers/userController.test.js
+++ src/tests/unit/controllers/userController.test.js
@@ -42,3 +42,42 @@ describe('Teste do controller criação de usuário', () => {
     });
   });
 });
+
+describe('Teste do controller para login', () => {
+  describe('Quando o usuário faz login com sucesso', () => {
+    const request = {
+      body: {
+        email: 'ja@gmail.com',
+        password: '123456',
+      },
+    };
+
+    const response = {
+      json: sinon.stub(),
+      status: sinon.stub(),
+    };
+
+    before(() => {
+      sinon.stub(userService, 'login').resolves();
+      response.status.returns(response);
+      userController.login(request, response);
+    });
+
+    after(() => {
+      userService.login.restore();
+    });
+
+    it('Retorna o código de status 200', () => {
+      expect(response.status.calledWith(200)).to.be.equal(true);
+    });
+
+    it('Retorna um objeto', () => {
+      expect(response.json.calledOnce).to.be.equal(true);
+    });
+
+    it('Retorna o objeto com um token', () => {
+      const [{ token }] = response.json.firstCall.args;
+      expect(token).to.be.a('string');
+    });
+  });
+});
