784007c61b8df3acf831243619ac223bfc4f8f2d
Jackson <jviana116@gmail.com>
2023-03-13 21:51:19 -0300

    fix: refatorando funcão createOrder no service

--- src/services/ordersService.js
+++ src/services/ordersService.js
@@ -2,21 +2,11 @@ const orderModel = require("../models/ordersModel");
 
 const createOrder = async (body, auth) => {
   const { paymentMethod, address, order } = body;
-  const orders = await Promise.all(order.map((item) => {
-    const { name, quantity, description, price } = item;
-    const orderList = {
-      name,
-      quantity,
-      description,
-      price,
-    }
-    return orderList;
-  }));
   const newOrder = await orderModel.create({
     user: auth,
     paymentMethod,
     address,
-    order: orders,
+    order,
   });
   return newOrder;
 };
